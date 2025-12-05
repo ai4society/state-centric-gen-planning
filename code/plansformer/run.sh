93ef9d905dffdd15cd780bde0028dafcad79ecb9
john <johnaaydin@gmail.com>
2025-12-05 12:27:20 -0500

    Fix binary path for plansformer validation

--- code/plansformer/run.sh
+++ code/plansformer/run.sh
@@ -2,12 +2,6 @@
 #SBATCH --job-name=pddl-dset
 #SBATCH --output=slurm/%x_%j.out
 #SBATCH --error=slurm/%x_%j.err
-# %x gives job name
-# %A Job array's master job allocation number.
-# %a Job array ID (index) number.
-# %j Job allocation number.
-# %N Node name. Only one file is created, so %N will be replaced by the name of the first node in the job, which is the one that runs the script.
-# %u User name.
 #SBATCH --mail-user=jaaydin@email.sc.edu
 #SBATCH --mail-type=ALL
 #SBATCH --partition=gpu
